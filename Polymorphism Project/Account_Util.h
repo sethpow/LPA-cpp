ea4da06050db99c819dbf089d4c7d39eeeaaad5e
Seth Powelson <sethmadspowelson@gmail.com>
2020-12-04 13:24:05 -0700

    added pointers and references in Util files

--- Polymorphism Project/Account_Util.h
+++ Polymorphism Project/Account_Util.h
@@ -2,14 +2,11 @@
 #define _ACCOUNT_UTIL_H_
 #include <vector>
 #include "Account.h"
-#include "Savings_Account.h"
-#include "Checking_Account.h"
-#include "Trust_Account.h"
 
-// Utility helper functions for Account class and Derived classes
+// Utility helper functions for Account * class and Derived classes
 
-virtual void display(const std::vector<Account *> &accounts);
-virtual void deposit(std::vector<Account *> &accounts, double amount);
-virtual void withdraw(std::vector<Account *> &accounts, double amount);
+void display(const std::vector<Account *> &accounts);
+void deposit(std::vector<Account *> &accounts, double amount);
+void withdraw(std::vector<Account *> &accounts, double amount);
 
 #endif
